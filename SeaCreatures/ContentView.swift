a66b00c84202b8ba4abb9ec699eb245f2cd75012
KellyChen <matijueming@gmail.com>
2024-10-17 22:21:45 +0800

    Initial Commit

---
+++ SeaCreatures/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SeaCreatures
+//
+//  Created by Jia Chen on 2024/10/17.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ SeaCreatures/SeaCreaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaCreaturesApp.swift
+//  SeaCreatures
+//
+//  Created by Jia Chen on 2024/10/17.
+//
+
+import SwiftUI
+
+@main
+struct SeaCreaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
